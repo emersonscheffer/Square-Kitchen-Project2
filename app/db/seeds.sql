f04a1a53cde971f5cd2cdd1a1f48e811d4ab9366
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-04-25 19:44:47 -0700

    added seeds

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -1,3 +1,60 @@
+
+
+INSERT INTO restaurants (rest_name, food_type, website_url, tel_number, rest_description, slot_num)
+VALUE ("Berkeley Pizza", "Italian", "www.berkeleypizza.com", "574 745 7366", "Best Pizza in Town", 4);
+
+INSERT INTO restaurants (rest_name, food_type, website_url, tel_number, rest_description, slot_num)
+VALUE ("Delhi Diner", "Indian", "www.indiandelhi.com", "510 111 1111", "Indian Bites", 3);
+
+INSERT INTO restaurants (rest_name, food_type, website_url, tel_number, rest_description, slot_num)
+VALUE ("Paris Bakery", "French", "www.parisbakery.com", "510 111 1111", "Best French", 5);
+
+INSERT INTO restaurants (rest_name, food_type, website_url, tel_number, rest_description, slot_num)
+VALUE ("Smokehouse Burgers", "American", "www.smokeburgers.com", "510 555 5555", "Burgers and Fries", 5);
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Pepperoni Pizza", 12.99, "Berkeley Pizza");
+
+
+
+
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Plain croissants", 3.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Almond croissants", 5.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Biscuit bakery with provolone cheese and ham or turkey", 7.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Brioche", 2.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("French baguette", 5.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Kalamata olive and Herbs", 4.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Walnut-raisins bread", 6.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Boule", 3.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Epis bread", 8.99, "Paris Bakery");
+
+INSERT INTO menu (item_name, item_price, rest_name)
+VALUE("Tarts with baked fruits", 5.99, "Paris Bakery");
+
+
+
+
+SELECT * FROM cart;
+SELECT * FROM menu;
+
 -- SELECT * FROM restaurants LIMIT 0,1;
 
 -- INSERT INTO restaurants (rest_name, food_type, website_url, tel_number, rest_description, slot_num)
